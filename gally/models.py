5ba6f895755b9f25ad6729997306fefbc436bf06
ines uwase <uwaseines10@gmail.com>
2019-10-10 20:51:08 +0200

    add category as class model

--- gally/models.py
+++ gally/models.py
@@ -2,16 +2,26 @@ from django.db import models
 
 # Create your models here.
 class Image(models.Model):
-    image = models.ImageField()
+    image_name = models.ImageField()
     name = models.CharField(max_length =30)
-    description= models.CharField(max_length =60)
-    location=models.ForeignKey(Location)
+    description = models.CharField(max_length =60)
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
 
     def __str__(self):
-        return self.first_name
+        return self.image_name
 
 class Location(models.Model):
     location = models.CharField(max_length =30)
 
     def __str__(self):
         return self.location
+
+class Category(models.Model):
+    category = models.CharField() 
+
+    def __str__(self):
+        return self.category     
+
+
+
