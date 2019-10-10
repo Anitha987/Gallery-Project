e9a6e803614ab3714a75720fbf73327b691a0afb
ines uwase <uwaseines10@gmail.com>
2019-10-10 20:00:59 +0200

    add a location model

--- gally/models.py
+++ gally/models.py
@@ -2,14 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Image(models.Model):
+    image = models.ImageField()
     name = models.CharField(max_length =30)
-    description= models.CharField(max_length =30)
-    # email = models.EmailField()
+    description= models.CharField(max_length =60)
+    location=models.ForeignKey(Location)
 
     def __str__(self):
         return self.first_name
 
-class location(models.Model):
+class Location(models.Model):
     location = models.CharField(max_length =30)
 
     def __str__(self):
