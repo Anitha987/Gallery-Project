717f30bd91bac20226980b62af1baabfceecadc8
ines uwase <uwaseines10@gmail.com>
2019-10-10 20:53:28 +0200

    test image class

--- gally/tests.py
+++ gally/tests.py
@@ -3,5 +3,9 @@ from .models import Image,Location,Category
 # Create your tests here.
 
 class ImageTestClass(TestCase):
-    
+    # Set up method
+    def setUp(self):
+        self.james= Editor(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
+
+
 
