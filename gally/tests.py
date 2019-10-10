64e8be4b00b0e98de3c1c40683a6648b6e9efafb
ines uwase <uwaseines10@gmail.com>
2019-10-10 21:14:02 +0200

    add an image instance

--- gally/tests.py
+++ gally/tests.py
@@ -2,7 +2,10 @@ from django.test import TestCase
 from .models import Image,Location,Category
 # Create your tests here.
 
-
+class ImageTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.image=Image(image='pussy.jpg',name='pussy',decription='pretty pussy',location='newyork',category='animals')
 
 
 
