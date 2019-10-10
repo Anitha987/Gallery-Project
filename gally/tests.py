0716523e8426f4b2bab6df0e5eedbe7c0ae36e7f
ines uwase <uwaseines10@gmail.com>
2019-10-10 20:59:27 +0200

    delete a test

--- gally/tests.py
+++ gally/tests.py
@@ -2,10 +2,8 @@ from django.test import TestCase
 from .models import Image,Location,Category
 # Create your tests here.
 
-class ImageTestClass(TestCase):
-    # Set up method
-    def setUp(self):
-        self.james= Editor(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
+
+
 
 
 
