d4bff44381a67dcfde7c053b5f60d813ceaa1745
ines uwase <uwaseines10@gmail.com>
2019-10-11 09:24:20 +0200

    add image field

--- gally/models.py
+++ gally/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Image(models.Model):
-    image = models.ImageField()
+    image = models.ImageField(null=True)
     name = models.CharField(max_length =30)
     description = models.CharField(max_length =60)
     # location = models.ForeignKey(Location)
