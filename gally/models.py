ad903ceb649f14ce8726580dcb13f6fb5d11d66a
ines uwase <uwaseines10@gmail.com>
2019-10-11 09:36:52 +0200

    add a saving function

--- gally/models.py
+++ gally/models.py
@@ -7,7 +7,9 @@ class Image(models.Model):
     description = models.CharField(max_length =60)
     # location = models.ForeignKey(Location)
     # category = models.ForeignKey(Category)
-
+    
+    def save_image(self):
+        self.save()
     def __str__(self):
         return self.image_name
 
